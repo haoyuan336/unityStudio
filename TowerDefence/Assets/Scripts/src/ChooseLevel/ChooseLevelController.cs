9c96e334df1a547c0678b9d1b1363599909a4bd7
楚浩远 <328387458@qq.com>
2018-08-29 21:21:53 +0800

    选择关卡的页面

---
+++ TowerDefence/Assets/Scripts/src/ChooseLevel/ChooseLevelController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseLevelController : MonoBehaviour
+{
+
+    // Use this for initialization
+    private Vector2 mousePosition = Vector2.zero;
+    public GameObject earthObject;
+    private bool isTouching = false;
+    private Vector3 targetRotation;
+    private Vector3 currentRotation = Vector3.zero;
+    public GameObject levelButtonPrefab;
+    void Start()
+    {
+        targetRotation = new Vector3(0, 0, 10);
+        for (int i = 0; i < 10; i ++){
+            GameObject obj = Instantiate(levelButtonPrefab);
+            obj.transform.parent = earthObject.transform;
+            Vector3 targetV = new Vector3(Random.Range(0, 100.0f), Random.Range(0, 100.0f), Random.Range(0, 100.0f));
+            //obj.transform.position = Vector3.Angle(Vector3.zero,);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f,1)){
+            if (hit.transform.tag == ""){
+
+            }
+        }
+
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = mousePos;
+            isTouching = true;
+
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mousePosition = mousePos;
+            isTouching = false;
+
+        }
+        if (isTouching == true)
+        {
+            //Vector2 mousePos = Input.mousePosition;
+            ////Debug.Log("moyse pos = " + mousePos);
+            //Vector2 director = mousePos - mousePosition;
+            //director = director.normalized * 2;
+            //mousePosition = mousePos;
+
+            //targetRotation += Vector3.down * director.x;
+            //targetRotation += Vector3.right * director.y;
+            //currentRotation = Vector3.MoveTowards(currentRotation, targetRotation.normalized, 0.1f);
+
+            //earthObject.transform.LookAt(currentRotation);
+            Vector2 director = mousePos - mousePosition;
+            mousePosition = mousePos;
+            earthObject.transform.Rotate(Vector3.down * director.x * Time.deltaTime * 10);
+        }
+    }
+}
---
+++ TowerDefence/Assets/Scripts/src/Game/Tower.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    //建造好之后 就可以追踪敌人了
+    // Use this for initialization
+    private EnemyController enemyController;
+    private List<GameObject> enemyList;
+    public float attackDistance;
+    private GameObject attackTarget;
+    private Vector3 targetVector;
+    private Vector3 currentVector;
+    public GameObject bulletPrefab;
+    public float shootDuraction;
+    private float shootNotTime = 0.0f;
+    public GameObject shootPoint;
+    public GameObject gun;
+    private void Awake()
+    {
+        currentVector = new Vector3(10, transform.position.y, 100);
+        //bulletPool = new ObjectPool(bulletPrefab, 3);
+    }
+    void Start()
+    {
+
+        //Debug.DrawLine(Vector3.zero, Vector3.up * 100,Color.red);
+        //Debug.DrawLine(Vector3.zero, new Vector3(100, 100, 100), Color.red);
+    }
+    public void SetController(Transform ctl)
+    {
+        enemyController = ctl.GetComponent<EnemyController>();
+        enemyList = enemyController.enemyList;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //取出父节点里面 的 所有敌人的节点
+        //遍历敌人节点 ，根据攻击距离寻找需要攻击的敌人 敌人列表不是空的，并且被攻击的对象 是null的 ，这时候寻找敌人
+        if (enemyList != null && attackTarget == null)
+        {
+            for (int i = 0; i < enemyList.Count; i++)
+            {
+                GameObject obj = enemyList[i];
+                if (!obj.transform.GetComponent<Enemy>().isDead())
+                {
+                    float dis = Vector3.Distance(transform.position, obj.transform.position);
+                    if (dis <= attackDistance)
+                    {
+                        attackTarget = obj;
+                    }
+                }
+
+            }
+        }
+        if (attackTarget != null && !attackTarget.GetComponent<Enemy>().isDead())
+        {
+            targetVector = new Vector3(attackTarget.transform.position.x, transform.position.y, attackTarget.transform.position.z);
+            currentVector = Vector3.MoveTowards(currentVector, targetVector, Vector3.Distance(currentVector, targetVector) * 0.2f);
+            transform.LookAt(currentVector);
+            float dis = Vector3.Distance(transform.position, attackTarget.transform.position);
+
+            if (dis > attackDistance)
+            {
+                attackTarget = null;
+            }
+        }
+        if (attackTarget != null && !attackTarget.transform.GetComponent<Enemy>().isDead())
+        {
+            if (shootNotTime > shootDuraction)
+            {
+                shootNotTime = 0;
+                ShootBullet();
+            }
+            else
+            {
+                shootNotTime += Time.deltaTime;
+                //攻击间隔
+            }
+
+        }else{
+            attackTarget = null;
+        }
+
+    }
+    void ShootBullet()
+    {
+        //攻击的时候 创建
+
+        Vector3 bulletPos = shootPoint.transform.position;
+        //GameObject bullet = bulletPool.GetObjFromPool(bulletPos);
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.transform.parent = transform.parent;
+        bullet.transform.position = bulletPos;
+        bullet.GetComponent<Bullet>().SetShootTarget(attackTarget);
+        //bullet.GetComponent<Bullet>().SetController(this);
+    }
+    public void CollisionAttack(GameObject enemyTarget)
+    {
+        //
+    }
+    public void BullletEnd(GameObject obj)
+    {
+        //bulletPool.ReccleObj(obj);
+    }
+}
